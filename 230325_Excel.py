a0ccdca93ee8a160b7b96cca54a395a1e4077730
GonoBae <qldrhqorhsh@naver.com>
2023-03-25 22:37:29 +0900

    commit test

--- 230325_Excel.py
+++ 230325_Excel.py
@@ -1,8 +1,16 @@
 import pandas
-import openpyxl
+import os
 
 
 excel_folder = '/Users/baegono/Desktop/Python/PythonBlog/ExcelFile'
 
-#print(pandas.read_excel(excel_folder + '/Programmer.xlsx', sheet_name=None).keys())
-#print(pandas.read_excel(excel_folder + '/Programmer.xlsx', sheet_name='Editor'))
+# print(pandas.read_excel(excel_folder + '/Programmer.xlsx', sheet_name=None).keys())
+# print(pandas.read_excel(excel_folder + '/Programmer.xlsx', sheet_name='Editor'))
+
+for path, dirs, files in os.walk(excel_folder):
+    for file in files:
+        if file.endswith('.xlsx'):
+            excel_file = pandas.ExcelFile(path + '/' + file)
+            for sheet in excel_file.sheet_names:
+                dataframe = excel_file.parse(sheet)
+                print(dataframe)
